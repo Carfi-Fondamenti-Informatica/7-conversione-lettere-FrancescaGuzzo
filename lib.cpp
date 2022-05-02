f82648c27196fcea151559e645c7d6d5449a9bf7
FrancescaGuzzo <100522082+FrancescaGuzzo@users.noreply.github.com>
2022-05-02 18:35:41 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,8 @@
 #include "lib.h"
 bool caratteri(int n){
-    bool x;
     if (((65 <= n)&&(n <= 90))||((97 <= n)&&(n <= 122))){
-        x=true;
+        return true;
     } else {
-        x=false;
+        return false;
     }
-    return x;
 }
